1c799fa787a5787e806e8ee9efcb5c6e7b8f698f
Paolo Bernardi Software Workshop <paolo@bernardi.cloud>
2025-04-30 14:58:58 +0200

    Titolo più corto

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>JobScanner - Scan your social accunts for embarassing posts before job interviews</title>
+    <title>JobScanner - Scan social accunts for embarassing posts before job interviews</title>
     <link rel="icon" type="image/png" href="/jobscanner/favicon-96x96.png" sizes="96x96">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <style>
